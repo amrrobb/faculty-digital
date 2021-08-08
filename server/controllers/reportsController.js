ac146e01c533e882f64da860bed5ff61deee291b
amrrobb <amr.robb@gmail.com>
2021-08-08 10:49:18 +0700

    serevr done

--- server/controllers/reportsController.js
+++ server/controllers/reportsController.js
@@ -1,50 +1,20 @@
 const {Report, sequelize} = require('../models')
 
-class Controller {
+class Controller {   
     static async findAllReports (req, res, next) {
         try {
-            const dataSales = await Controller.findSalesReports(req, res, next)
-            const dataExpenses = await Controller.findExpensesReports(req, res, next)
-
-            res.status(200).json({dataSales, dataExpenses})
-
-        }
-        catch(err) {
-            next({code: 500, message: err.message})
-        }
-    }
-
-    static async findSalesReports (req, res, next) {
-        try {
             const data = await Report.findAll({
-                where: {type: "sales"},
                 attributes: [
                     [sequelize.fn('DATE_TRUNC', 'month', sequelize.col('createdAt')), 'month'], 
-                    [sequelize.fn('SUM', sequelize.col('amount')), 'totalSales']
+                    [sequelize.fn('SUM', sequelize.literal("CASE WHEN type = 'sales' THEN amount ELSE 0 END")), 'totalSales'],
+                    [sequelize.fn('SUM', sequelize.literal("CASE WHEN type = 'expenses' THEN amount ELSE 0 END")), 'totalExpenses'],
                 ],
-                group: [sequelize.fn('date_trunc', 'month', sequelize.col('createdAt'))]
+                group: [sequelize.fn('date_trunc', 'month', sequelize.col('createdAt'))],
+                order: [[sequelize.fn('date_trunc', 'month', sequelize.col('createdAt')), 'DESC']]
             })
+            res.json(data)
             return data
             
-        }
-        catch(err) {
-            next({code: 500, message: err.message})
-        }
-    }
-    
-    static async findExpensesReports (req, res, next) {
-        try {
-            const data = await Report.findAll({
-                where: {type: "expenses"},
-                attributes: [
-                    [sequelize.fn('DATE_TRUNC', 'month', sequelize.col('createdAt')), 'month'], 
-                    [sequelize.fn('SUM', sequelize.col('amount')), 'totalExpenses']
-                ],
-                group: [sequelize.fn('date_trunc', 'month', sequelize.col('createdAt'))]
-            })
-            
-            return data
-
         }
         catch(err) {
             next({code: 500, message: err.message})
